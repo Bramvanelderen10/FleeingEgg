d2b1f4113c8c9ac2c1f7db3e9386bec8de71d0f9
Bram van Elderen <bramvanelderen1994@gmail.com>
2016-07-12 23:52:10 +0200

    Difficulty class added

---
+++ Assets/Scripts/DifficultyManager.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+
+public class DifficultyManager {
+    public static DifficultyManager Instance;
+
+    public DifficultyManager()
+    {
+        Instance = this;
+    }
+
+}
